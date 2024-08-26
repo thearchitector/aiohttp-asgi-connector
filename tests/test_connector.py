318888268dae3a4debe47667c68c2418b0fa5532
Daniël van de Giessen <daniel@dvdgiessen.nl>
2024-08-26 16:39:41 +0200

    fix: disconnect request after completion

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,4 +1,7 @@
 import pytest
+from aiohttp import ClientSession
+
+from aiohttp_asgi_connector import ASGIApplicationConnector
 
 
 async def test_bad_method(session):
@@ -30,3 +33,22 @@ async def test_app_failure_propagate(session):
     with pytest.raises(Exception, match="something bad happened"):
         async with session.get("/fail?handle=false") as resp:
             assert await resp.json()
+
+
+async def test_disconnect_after_response_sent():
+    async def app(scope, receive, send):
+        while (await receive()).get("more_body", False):
+            pass
+        await send({"type": "http.response.start", "status": 204, "headers": []})
+        await send({"type": "http.response.body", "body": b"", "more_body": False})
+
+        # After the response is sent we should get a 'disconnected' event, see
+        # https://asgi.readthedocs.io/en/latest/specs/www.html#disconnect-receive-event
+        assert (await receive()).get("type") == "http.disconnect"
+
+    async with ClientSession(connector=ASGIApplicationConnector(app)) as session:
+        async with session.post(
+            "http://localhost/",
+            data=b"hello world",
+        ) as resp:
+            assert resp.status == 204
