8e718082e34039f522fff7f8a049f186e55a13ff
Elias Gabriel <oss@eliasfgabriel.com>
2026-09-23 18:39:41 -0400

    fix: signal protocol on explicit close

--- tests/test_connector.py
+++ tests/test_connector.py
@@ -1,51 +1,65 @@
 import json
+from typing import Any, Awaitable, Callable, Dict, MutableMapping
 
 import pytest
 from aiohttp import ClientSession, ClientTimeout
+from fastapi import FastAPI
 
 from aiohttp_asgi_connector import ASGIApplicationConnector
 
 
-async def test_app_failure_handled(session):
+async def test_app_failure_handled(session: ClientSession) -> None:
     async with session.get("/fail?handle=true") as resp:
         assert await resp.json()
 
 
-async def test_app_failure_propagate(session):
-    with pytest.raises(Exception, match="something bad happened"):
+async def test_app_failure_propagate(session: ClientSession) -> None:
+    with pytest.raises(RuntimeError, match="something bad happened"):
         async with session.get("/fail?handle=false") as resp:
             assert await resp.json()
 
 
-async def test_bad_method(session):
+async def test_bad_method(session: ClientSession) -> None:
     async with session.post("/get") as resp:
         assert (await resp.json()) == {"detail": "Method Not Allowed"}
 
 
-async def test_get(session):
+async def test_get(session: ClientSession) -> None:
     async with session.get("/get") as resp:
         assert (await resp.json()) is True
 
 
-async def test_post_json(session):
+async def test_post_json(session: ClientSession) -> None:
     async with session.post("/post_json", json={"message": "hello world"}) as resp:
         assert (await resp.json()) == {"broadcast": "hello world"}
 
 
-async def test_post_form(session):
+async def test_close_session_with_unread_response(application: FastAPI) -> None:
+    async with ClientSession(
+        connector=ASGIApplicationConnector(application), base_url="http://localhost"
+    ) as session:
+        resp = await session.post("/post_json", json={"message": "x" * 2**20})
+        assert resp.status == 200
+
+
+async def test_post_form(session: ClientSession) -> None:
     async with session.post("/post_form", data={"message": "hello world"}) as resp:
         assert (await resp.json()) == {"broadcast": "hello world"}
 
 
-async def test_app_stream(session):
+async def test_app_stream(session: ClientSession) -> None:
     async with session.get("/stream", timeout=ClientTimeout(total=3)) as resp:
         content = [i async for i in resp.content.iter_any()]
         assert json.loads(b"".join(content).decode())
         assert len(content) == 4
 
 
-async def test_disconnect_after_response_sent():
-    async def app(scope, receive, send):
+async def test_disconnect_after_response_sent() -> None:
+    async def app(
+        scope: Dict[str, Any],
+        receive: Callable[[], Awaitable[Dict[str, Any]]],
+        send: Callable[[MutableMapping[str, Any]], Awaitable[None]],
+    ) -> None:
         while (await receive()).get("more_body", False):
             pass
 
@@ -56,6 +70,7 @@ async def test_disconnect_after_response_sent():
         # https://asgi.readthedocs.io/en/latest/specs/www.html#disconnect-receive-event
         assert (await receive()).get("type") == "http.disconnect"
 
-    async with ClientSession(connector=ASGIApplicationConnector(app)) as session:
-        async with session.post("http://localhost") as resp:
-            assert resp.status == 204
+    async with ClientSession(
+        connector=ASGIApplicationConnector(app)
+    ) as session, session.post("http://localhost") as resp:
+        assert resp.status == 204
